f50927f9c8d45a5690e6b8ba5360190ca9701d53
Remesoft <email.dwip@gmail.com>
2026-01-04 01:49:12 +0600

    chat background done

--- src/features/chat/components/ChatArea.tsx
+++ src/features/chat/components/ChatArea.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import TextMessage from "@/components/TextMessage";
 import { setMediaFrom } from "@/features/state";
 import { useAppDispatch, useAppSelector } from "@/hooks";
+import chatBackground from "@/assets/chat-background.svg";
 
 const ChatArea: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -17,16 +18,20 @@ const ChatArea: React.FC = () => {
   const isMediaOpen = Boolean(mediaFrom);
 
   return (
-    <main className="bg-[#EFEEF3] flex-grow relative overflow-hidden pb-[80px]">
+    <main className="relative flex-grow overflow-hidden pb-[80px] bg-[#EFEEF3]">
+      {/* Background */}
+      <div className="absolute inset-0 z-0 bg-[url('@/assets/chat-background.svg')] bg-repeat bg-contain bg-center opacity-[0.05] pointer-events-none" />
+
       {/* Media overlay */}
       {isMediaOpen && (
         <div
           onClick={() => dispatch(setMediaFrom(null))}
-          className="absolute top-0 z-1 h-full w-full bg-black/20 backdrop-blur-[2px]"
+          className="absolute inset-0 z-10 bg-black/20 backdrop-blur-[2px]"
         />
       )}
 
-      <div className="flex flex-col h-full overflow-y-auto px-5">
+      {/* Chat content */}
+      <div className="relative flex flex-col h-full overflow-y-auto px-5">
         <TextMessage message="Message One" />
 
         {Array.from({ length: 40 }).map((_, index) => (

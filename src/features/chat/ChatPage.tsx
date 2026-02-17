02ef7a7404f612c6120c027e2e222b852f9a0ca1
Remesoft <email.dwip@gmail.com>
2026-02-17 06:15:13 +0600

    information setup done

--- src/features/chat/ChatPage.tsx
+++ src/features/chat/ChatPage.tsx
@@ -1,31 +1,43 @@
 import React from 'react';
+import { Navigate, useParams } from 'react-router';
+
 import AuthSheet from '../auth/AuthSheet';
 import Header from './components/Header';
 import ChatArea from './components/ChatArea';
 import InputArea from './components/InputArea';
 import { useReceivedMessage } from './hooks/useReceivedMessage';
-import { Navigate, useParams } from 'react-router';
 import { useUserInfoQuery } from '../auth/api';
 import { useLoadMessagesQuery } from './api';
 
 const ROOM_ID = 'client_3';
-const ChatPage: React.FC = () => {
-  useReceivedMessage({ roomId: ROOM_ID });
-  
-  // get username
-  const { username } = useParams<{ username: string }>();
-  if (!username) return <Navigate to="/404" replace />;
-  
-  // Fetch owner info
-  useUserInfoQuery(username);
-  useLoadMessagesQuery({clientId: 1, userId: 1})
 
+const ChatPage: React.FC = () => {
+  // -------------------------------------
+  // Hooks
+  // -------------------------------------
+  const { username } = useParams<{ username: string }>();
+  const { data: user, isError, isLoading } = useUserInfoQuery(username ?? '');
+  useReceivedMessage({ roomId: ROOM_ID });
+
+  // Load messages once client exists
+  useLoadMessagesQuery({ clientId: 1, userId: 1 });
+
+  // -------------------------------------
+  // Redirect if username is missing or user not found
+  // -------------------------------------
+  if (!username || isError || (!isLoading && !user)) {
+    return <Navigate to="/404" replace />;
+  }
+
+  // -------------------------------------
+  // Render Chat Page
+  // -------------------------------------
   return (
     <div className="h-[100dvh] w-screen flex flex-col relative overflow-hidden">
       <Header />
       <ChatArea />
       <InputArea />
-      <AuthSheet /> 
+      <AuthSheet />
     </div>
   );
 };

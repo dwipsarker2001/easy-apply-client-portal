f4e014cc07225d87b4933b958ddf739488ce52fd
dwipsarker2001 <email.dwip@gmail.com>
2026-04-17 00:05:22 +0600

    Socket base url updated

--- src/features/chat/hooks/useSocket.tsx
+++ src/features/chat/hooks/useSocket.tsx
@@ -5,12 +5,13 @@ import { useAppSelector } from '@/hooks';
 export const useSocket = () => {
   const [socket, setSocket] = useState<Socket | null>(null);
   const token = useAppSelector(state => state.auth.clientToken);
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
 
   useEffect(() => {
     if (!token) return;
 
     // connection setup
-    const newSocket = io('http://localhost:8080', {
+    const newSocket = io(BASE_URL, {
       withCredentials: true,
       auth: { token: token },
     });
